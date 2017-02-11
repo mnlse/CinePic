b105054ced2a6980c9da76549b1a1bf7c799ccb7
mnlse <nielsenmateusz@gmail.com>
2017-02-11 11:00:02 +0100

    ADDED multiple ways to display user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_display_name
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_display_name
+    @user = current_user
+    if @user.first_name? && @user.last_name?
+      @name = @user.first_name + " " + @user.last_name
+    elsif @user.username?
+      @name = @user.username
+    else
+      @name = @user.email
+    end
+  end
 
   def configure_permitted_parameters
     update_attrs = [:bio, :email, :avatar, :first_name, :last_name, :username]
