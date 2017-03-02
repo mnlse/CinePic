71a0f3c0919cfbb5d0a2d4dd7b49f4b2307de0c9
mnlse <nielsenmateusz@gmail.com>
2017-03-02 23:35:34 +0100

    IMPROVED user profile - working, styled ratings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,35 @@
 class UsersController < ApplicationController
   before_action :find_user, only: [:show]
+  require 'ostruct'
+
+  def prepRatings(ratingsAssoc)
+    arr = []
+    ratingsAssoc.each do |rating|
+      obj = OpenStruct.new
+      mov = Movie.find(rating.movie_id)
+      obj[:mov_title] = mov.title
+      obj[:rating] = rating.rating
+      obj[:thoughts] = rating.thoughts
+      obj[:rating_date] = rating.updated_at
+      obj[:mov_year] = mov.release_date.year
+      obj[:mov_cover_url] = mov.cover_img.url(:front_page)
+      obj[:mov_url] = movie_path(rating.movie_id)
+
+      arr.push(obj)
+    end
+    arr
+  end
 
   def show
-    @ratings = Rating.where(user_id: @user.id)
-    @comments = Comment.where(user_id: @user.id)
+    ratings = Rating.where(user_id: params[:id]).last(10).reverse
+    @recently_rated = prepRatings(ratings)
+
+    ratings = Rating.where(user_id: params[:id]).order("rating DESC").first(10)
+    @highest_rated = prepRatings(ratings)
+
+
+
+    @comments = Comment.where(user_id: params[:id])
   end
 
   private
